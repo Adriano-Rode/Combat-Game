4c712f91c90f761e1915475fd814bc0f13807e21
Djanhinho <feliperodeadriano@outlook.com>
2024-04-27 14:06:58 -0300

    First version of player movement based on camera position

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -4,19 +4,30 @@ public class PlayerMovements : MonoBehaviour
 {
     private CharacterController _controller;
     private float _playerSpeed = 4f;
+    public Transform cam;
 
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
     }
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        _controller.Move(move * Time.deltaTime * _playerSpeed);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        camForward.y = 0f;
+        camRight.x = 0f;
+
+        Vector3 forward = vertical * camForward;
+        Vector3 right = horizontal * camRight;
+
+        Vector3 move = forward + right;
+
+        _controller.Move(move.normalized * Time.deltaTime * _playerSpeed);
     }
+    
 }
